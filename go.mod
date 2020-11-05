b3a0e058d39b8d4077ad88dd7639a8428099747d
Siôn le Roux <sinisterstuf@gmail.com>
2020-11-05 18:25:46 +0100

    Upgrade to Ebiten version 2
    This is a breaking change so the start code boilerplate needs to be
    changed too.  I'm happy that instead of the check for whether we're
    rendering in this loop or not we have 2 separate functions for update
    and draw.  I've modified the code accordingly and it start up fine.

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/jatekalkotok/lunar-defence
 
 go 1.13
 
-require (
-	github.com/go-gl/glfw v0.0.0-20190409004039-e6da0acd62b1 // indirect
-	github.com/hajimehoshi/ebiten v1.12.3
-	github.com/kr/pretty v0.1.0 // indirect
-)
+require github.com/hajimehoshi/ebiten/v2 v2.0.0
--- main.go
+++ main.go
@@ -4,26 +4,36 @@ import (
 	"errors"
 	"log"
 
-	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Lunar Defence"); err != nil {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Lunar Defence")
+	game := &Game{}
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func update(screen *ebiten.Image) error {
+// Game represents the main game state
+type Game struct{}
 
+// Update calculates game logic
+func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
 		return errors.New("game quit by player")
 	}
 
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-
-	// TODO: draw stuff here
-
 	return nil
+}
+
+// Draw handles rendering the sprites
+func (g *Game) Draw(screen *ebiten.Image) {
+	// TODO: draw stuff here
+}
+
+// Layout is hardcoded for now, may be made dynamic in future
+func (g *Game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
+	return 640, 480
 }
