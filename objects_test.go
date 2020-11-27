7c4a5050b8cab039f42d63833bd1bdde2a53b1e2
Siôn le Roux <sinisterstuf@gmail.com>
2020-11-27 16:49:11 +0100

    Set up a first unit test
    This test doesn't do anything useful yet except to show that test
    running works fine and the CI pipeline can handle it correctly.
    Consider it a dummy that needs to be replaced with some useful test
    cases.  I picked overlaps function because it's used for collision
    detection for all the objects, so it would be pretty criritcal to verify
    that it's working right.

---
+++ objects_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestOverlaps(t *testing.T) {
+	object := NewObject("/asteroid.png")
+	if object == nil {
+		t.Logf("could't even make an object...")
+	}
+	// TODO: write real collision test cases here, the above is just to stop the
+	// compiler complaining about unused vars etc
+}
